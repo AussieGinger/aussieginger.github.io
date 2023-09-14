390a5867cf0479bd449a0daad20c0c20b1da403f
AussieGinger <JoshBurns2307@gmail.com>
2023-09-14 21:53:07 +1000

    Added Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}",
+            "./bat-types/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
